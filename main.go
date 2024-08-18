96c5b8b69cd870889b8e24a372275f2e49ae335f
ojembs <awejoseph01@gmail.com>
2024-08-19 00:42:03 +0100

    Pacjage scope

--- main.go
+++ main.go
@@ -5,19 +5,11 @@ import (
 )
 
 func main() {
+   sayHello("Joe")
    
+   for _, v := range points {
+    fmt.Println(v)
+   }
 
-    names := [4]string{"joe", "dodo", "pizza", "testing"}
-
- 
-    for index, value := range names {
-        if index == 1 {
-            fmt.Println("Continue at index", index)
-            // continue
-        } else if index == 2 {
-            fmt.Printf("The name of the person at index %v is %v \n", index, value)
-        } else {
-            fmt.Println("We are done")
-        }
-    }
+//    sorting := 
 }
